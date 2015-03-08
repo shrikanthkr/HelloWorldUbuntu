369b523378aae04e46765122eb9b0908f25cb679
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-03-08 11:23:12 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QApplication>
+#include <QDesktopWidget>
+#include "html5applicationviewer.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    int height,width;
+
+    Html5ApplicationViewer viewer;
+    viewer.setOrientation(Html5ApplicationViewer::ScreenOrientationAuto);
+    viewer.showExpanded();
+    QRect rec = QApplication::desktop()->screenGeometry();
+     height = rec.height();
+     width = rec.width();
+    viewer.resize(width,height);
+    viewer.loadFile(QLatin1String("html/index.html"));
+
+    return app.exec();
+}
